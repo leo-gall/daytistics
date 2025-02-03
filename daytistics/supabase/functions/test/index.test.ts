da1224b343bbb915b14ffd7a290441555cd17eec
Leo Gall <gall.dev@proton.me>
2025-02-03 15:39:44 +0100

    wip

--- daytistics/supabase/functions/test/index.test.ts
+++ daytistics/supabase/functions/test/index.test.ts
@@ -3,10 +3,9 @@ import {
   assertEquals,
 } from "https://deno.land/std@0.192.0/testing/asserts.ts";
 import { createClient, SupabaseClient } from "jsr:@supabase/supabase-js@2";
-console.log("1" + JSON.stringify(Deno.env.toObject(), null, 2));
 
 const testHelloWorld = async () => {
-  const supabaseUrl = Deno.env.get("SUPABASE_URL") ?? "";
+  const supabaseUrl = Deno.env.get("SUPABASE_API_URL") ?? "";
   const supabaseKey = Deno.env.get("SUPABASE_ANON_KEY") ?? "";
   const options = {
     auth: {
@@ -15,7 +14,12 @@ const testHelloWorld = async () => {
       detectSessionInUrl: false,
     },
   };
-  console.log("2" + JSON.stringify(Deno.env.toObject(), null, 2));
+  const filteredEnv = Object.fromEntries(
+    Object.entries(Deno.env.toObject()).filter(([key]) =>
+      key.startsWith("SUPABASE_")
+    )
+  );
+  console.log(JSON.stringify(filteredEnv, null, 2));
   var client: SupabaseClient = createClient(supabaseUrl, supabaseKey, options);
 
   // Invoke the 'hello-world' function with a parameter
