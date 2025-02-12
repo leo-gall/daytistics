ffda7b9244dd7c2bb2bdeefb41a62dad9508ad23
Leo Gall <gall.dev@proton.me>
2025-02-12 06:23:33 +0100

    add additional debugging

--- daytistics/supabase/functions/tests/send_conversation_message.test.ts
+++ daytistics/supabase/functions/tests/send_conversation_message.test.ts
@@ -52,6 +52,8 @@ async function testWithoutConversationId(supabase: SupabaseClient, date: Date) {
 
   if (response.data?.error) {
     throw new Error(`Something went wrong: ${response.data?.error}`);
+  } else {
+    console.log(`Response is ${response}`);
   }
 
   const conversation: DatabaseConversation = (
