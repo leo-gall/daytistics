e941df5349a2acf44e9d8dc6a2df39bf5ddfebf0
Leo Gall <gall.dev@proton.me>
2025-03-01 06:04:44 +0100

    fix: update plugins config, to solve posthog init error

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -13,7 +13,8 @@ export default defineNuxtConfig({
     },
   },
 
-  // set favicon
+  plugins: [{ src: "./plugins/posthog", mode: "client" }],
+
   app: {
     head: {
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
