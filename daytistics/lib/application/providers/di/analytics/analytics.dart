8d80c43f24aede131c0e3faafab0b937d380c97f
Leo Gall <gall.dev@proton.me>
2025-05-01 07:29:27 +0200

    refactor: fix openpanel issues

--- daytistics/lib/application/providers/di/analytics/analytics.dart
+++ daytistics/lib/application/providers/di/analytics/analytics.dart
@@ -1,8 +1,7 @@
 // ignore: depend_on_referenced_packages
-import 'dart:convert';
-import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'package:openpanel_flutter/openpanel_flutter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'analytics.g.dart';
@@ -14,38 +13,7 @@ class Analytics {
     required String eventName,
     Map<String, dynamic>? properties,
   }) async {
-    try {
-      final Map<String, dynamic> deviceInfo = {
-        'platform': Platform.operatingSystem,
-        'platform_version': Platform.operatingSystemVersion,
-        'device': Platform.localHostname,
-      };
-
-      final response = await httpClient.post(
-        Uri.parse('${const String.fromEnvironment('OPENPANEL_URL')}/track'),
-        headers: {
-          'Content-Type': 'application/json',
-          'user-agent': jsonEncode(deviceInfo),
-          'openpanel-client-id':
-              const String.fromEnvironment('OPENPANEL_CLIENT_ID'),
-          'openpanel-client-secret':
-              const String.fromEnvironment('OPENPANEL_CLIENT_SECRET'),
-        },
-        body: jsonEncode({
-          'type': 'track',
-          'payload': {
-            'name': eventName,
-            'properties': properties,
-          },
-        }),
-      );
-
-      if (response.statusCode != 200) {
-        throw Exception('Failed to track event');
-      }
-    } on SocketException catch (e) {
-      throw Exception(e.message);
-    }
+    Openpanel.instance.event(name: eventName, properties: properties ?? {});
   }
 }
 
--- daytistics/lib/initializers.dart
+++ daytistics/lib/initializers.dart
@@ -2,6 +2,7 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:daytistics/config/settings.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:openpanel_flutter/openpanel_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> initSupabase() async {
@@ -25,5 +26,14 @@ Future<bool> initAwesomeNotifications() {
       ),
     ],
     debug: kDebugMode,
+  );
+}
+
+Future<void> initOpenpanel() async {
+  await Openpanel.instance.initialize(
+    options: OpenpanelOptions(
+      clientId: const String.fromEnvironment('OPENPANEL_CLIENT_ID'),
+      clientSecret: const String.fromEnvironment('OPENPANEL_CLIENT_SECRET'),
+    ),
   );
 }
