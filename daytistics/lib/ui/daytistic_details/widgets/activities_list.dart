95ae9658d69a7750703be3f04b98e209e3cb3994
Leo Gall <gall.dev@proton.me>
2025-05-02 19:08:16 +0200

    fix: add `SingleChildScrollView` to activities list to make it scrollable again

--- daytistics/lib/ui/daytistic_details/widgets/activities_list.dart
+++ daytistics/lib/ui/daytistic_details/widgets/activities_list.dart
@@ -10,34 +10,36 @@ class ActivitiesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: daytistic != null ? daytistic?.activities.length : 0,
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            title: Text(
-              '${dateTimeToHourMinute(daytistic!.activities[index].startTime)} - ${dateTimeToHourMinute(daytistic!.activities[index].endTime)}',
-            ),
-            subtitle: Text(daytistic!.activities[index].name),
-            trailing: IconButton(
-              onPressed: () => EditActivityDialog.showDialog(
-                context,
-                daytistic!.activities[index],
+    return SingleChildScrollView(
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: daytistic != null ? daytistic?.activities.length : 0,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(
+                '${dateTimeToHourMinute(daytistic!.activities[index].startTime)} - ${dateTimeToHourMinute(daytistic!.activities[index].endTime)}',
               ),
-              icon: const Icon(Icons.edit_outlined),
+              subtitle: Text(daytistic!.activities[index].name),
+              trailing: IconButton(
+                onPressed: () => EditActivityDialog.showDialog(
+                  context,
+                  daytistic!.activities[index],
+                ),
+                icon: const Icon(Icons.edit_outlined),
+              ),
+              contentPadding: const EdgeInsets.only(
+                left: 20,
+                top: 2,
+                right: 5,
+                bottom: 2,
+              ),
             ),
-            contentPadding: const EdgeInsets.only(
-              left: 20,
-              top: 2,
-              right: 5,
-              bottom: 2,
-            ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
